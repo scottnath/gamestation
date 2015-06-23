fbfd0cc717520fad3e21c8f2d73a9f87da26e5c2
Scott Nath <git@scottnath.com>
2015-06-23 03:48:40 +0000

    basic server and html

---
+++ index.js
@@ -0,0 +1,48 @@
+var express = require('express'),
+	path = require('path'),
+	cors = require('cors'),
+	bodyParser = require('body-parser'),
+	exec = require('child_process').exec;
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname,'/public/index.html'));
+});
+
+app.use('/public', express.static('public'));
+
+// app.use('/game/:id', function (req, res, next) {
+//   console.log('Request Type:', req.params);
+
+//   next();
+// });
+
+app.post('/game/:filename', function(req, res){
+	var gamefilename = req.params.filename;
+	console.log('body: ' + JSON.stringify(req.params));
+	console.log('gameid: ' + gamefilename);
+	gameKicker(gamefilename);
+	res.send(req.body);
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('GameStation listening at fuckin http://%s:%s', host, port);
+
+});
+
+function gameKicker(gamefilename){
+	exec('/opt/retropie/emulators/retroarch/bin/retroarch -L /opt/retropie/libretrocores/lr-imame4all/libretro.so /home/pi/RetroPie/roms/mame-mame4all/' + gamefilename + '.zip', function(error, stdout, stderr) {
+	  if (!error) {
+	    console.log('// things worked!');
+	  } else {
+	    console.log('// things failed :(');
+	  }
+	});
+}
